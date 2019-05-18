2de71ece11cd7a0aa386cc694b22e17e38d9afeb
Sean <shonkelli@gmail.com>
2019-05-18 12:24:56 -0400

    refactored code, though this does not appear to improve performance

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -11,24 +11,52 @@ class RecurringMoment
   end
 
   def match(date)
+    @date = date
 
-    current = @start
+    case @period
+      when 'monthly'
+        return increment_month
+      when 'weekly'
+        return increment_week
+      when 'daily'
+        return increment_day
+    end
+
+  end
+
+  def increment_month
     i = 1
-
-    while current <= date
-      if current == date
+    current = @start
+    while current <= @date
+      if current == @date
         return true
       end
-      case @period
-        when 'monthly'
-          current = @start.advance(months: @interval * i)
-          i += 1
-        when 'weekly'
-          current = current.advance(weeks: @interval)
-        when 'daily'
-          current = current.advance(days: @interval)
-      end
+      current = @start.advance(months: @interval * i)
+      i += 1
     end
     return false
   end
+
+  def increment_week
+    current = @start
+    while current <= @date
+      if current == @date
+        return true
+      end
+      current = current.advance(weeks: @interval)
+    end
+    return false
+  end
+
+  def increment_day
+    current = @start
+    while current <= @date
+      if current == @date
+        return true
+      end
+      current = current.advance(days: @interval)
+    end
+    return false
+  end
+
 end
